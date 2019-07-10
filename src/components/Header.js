1f3c4909de5b618eff72167d5fa104ea7d9f4d00
Yi <mavisluan83@gmail.com>
2019-07-09 21:01:11 -0700

    create Header component

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+import { Navbar, Nav, Form } from 'react-bootstrap'
+
+const Header = props => {
+    return (
+        <div style={{ background: "#ff8000" }} className="text-white text-center p-4">
+            <Navbar fixed="top" variant="dark" >
+                <Nav className="ml-auto small font-weight-bolder">
+                    <Nav.Link href="/">HOME</Nav.Link>
+                    <Nav.Link href="/favorite">FAVORITE</Nav.Link>
+                </Nav>
+            </Navbar>
+            <div style={{fontSize: "2rem"}} className="title">The Beer Bank</div>
+            <div className="small mb-3">Find your favorite beer here</div>
+            <Form className="col-8 mx-auto">
+                <Form.Control placeholder="Search for beer name" />
+            </Form>
+        </div>
+    )
+}
+
+Header.propTypes = {
+
+}
+
+export default Header

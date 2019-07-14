bebc238d3bc495c31a86296e168b88b7f13416b2
Yi <mavisluan83@gmail.com>
2019-07-14 13:17:21 -0700

    remove actions and reducers

--- src/actions/query.js
+++
@@ -1,9 +0,0 @@
-import * as API from '../BeersAPI';
-
-export const LOAD_QUERY_ITEMS = 'LOAD_QUERY_ITEMSY';
-export const loadQuery = query => ({
-    type: 'LOAD_QUERY',
-    query,
-});
-
-export const fetchQuery = () => async dispatch => {};
--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import itemsReducer from './itemsReducer';
-import modalItemReducer from './modalItemReducer';
-
-const rootReducer = combineReducers({ items: itemsReducer, modalItem: modalItemReducer });
-
-export default rootReducer;
